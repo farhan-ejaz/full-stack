456eb75c4c67590b815b20523a058170bae21029
Mohammed Farhan <mohammed.farhan1@se.com>
2024-03-02 21:11:31 +0530

    test

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+//import "./todo/todo";
+
+//import workletURL from './todo/todo.js?url'
